0655543ee17a5952e74f5bdd131991aaf3df8903
wangkobong <61865579+wangkobong@users.noreply.github.com>
2021-04-20 23:19:48 +0900

    add editor and preview components

---
+++ src/components/preview/preview.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './preview.module.css';
+
+const Preview = (props) => <section className={styles.preview}>
+<h1 className={styles.title}>Care Preview</h1>
+</section>
+
+export default Preview;
