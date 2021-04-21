85e26234dc4081e92e0f52fde18eeb89e63d04c1
wangkobong <61865579+wangkobong@users.noreply.github.com>
2021-04-21 22:38:51 +0900

    add card and edit form component

---
+++ src/components/card_add_form/card_add_form.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import styles from './card_add_form.module.css';
+
+const CardAddForm = () => <h1>1</h1> ;
+
+
+export default CardAddForm;
--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router';
 import Editor from '../editor/editor';
 import Footer from '../footer/footer';
@@ -7,6 +7,38 @@ import Preview from '../preview/preview';
 import styles from './maker.module.css';
 
 const Maker = ({authService}) => {
+  const [cards, setCards] = useState([
+    {
+      id: '1',
+      name: 'ellie',
+      company: 'samsung',
+      theme: 'colorful',
+      email: 'ellie@gmail.com',
+      messge: 'go for it',
+      fileName: 'ellie',
+      fileURL: 'ellie.png',
+    },
+    {
+      id: '2',
+      name: 'ellie2',
+      company: 'samsung',
+      theme: 'dark',
+      email: 'ellie@gmail.com',
+      messge: 'go for it',
+      fileName: 'ellie',
+      fileURL: null,
+    },
+    {
+      id: '3',
+      name: 'ellie3',
+      company: 'samsung',
+      theme: 'light',
+      email: 'ellie@gmail.com',
+      messge: 'go for it',
+      fileName: 'ellie',
+      fileURL: null,
+    },
+  ]);
   const history = useHistory();
   const onLogout = () =>{
     authService.logout();
@@ -21,13 +53,13 @@ const Maker = ({authService}) => {
     });
   })
 
-
+  console.log(cards);
   return (  
     <section className={styles.maker}>
       <Header onLogout={onLogout}/>
       <div className={styles.container}>
-        <Editor />
-        <Preview />
+        <Editor cards={cards}/>
+        <Preview cards={cards}/>
       </div>
       <Footer />
     </section>
