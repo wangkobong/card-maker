760896ae3c0e1d60b7ed0b08cf282950a8a45779
wangkobong <61865579+wangkobong@users.noreply.github.com>
2021-04-22 02:03:31 +0900

    handle image uploading

--- src/components/card_edit_form /card_edit_form.jsx
+++ src/components/card_edit_form /card_edit_form.jsx
@@ -1,9 +1,8 @@
 import React, { useRef } from 'react';
 import Button from '../button/button';
-import ImageFileInput from '../image_file_input/image_file_input';
 import styles from './card_edit_form.module.css';
 
-const CardEditForm = ({ card, updateCard, deleteCard }) => {
+const CardEditForm = ({ FileInput, card, updateCard, deleteCard }) => {
   const {name, company, title, email, message, theme, fileName, fileURL} = card;
   const formRef = useRef();
   const nameRef = useRef();
@@ -13,6 +12,14 @@ const CardEditForm = ({ card, updateCard, deleteCard }) => {
   const emailRef = useRef();
   const messageRef = useRef();
 
+
+  const onFileChange = file => {
+    updateCard({
+      ...card,
+      fileName: file.name,
+      fileURL: file.url,
+    })
+  }
   const onChange = (event) => {
     if (event.currentTarget == null ){
       return;
@@ -42,7 +49,7 @@ const CardEditForm = ({ card, updateCard, deleteCard }) => {
       <input ref={emailRef} className={styles.input} type="text" name="email" value ={email} onChange={onChange}/>
       <textarea ref={messageRef} className={styles.textarea} name="message" value={message} onChange={onChange}></textarea>
       <div className={styles.fileinput}>
-        <ImageFileInput />
+        <FileInput name={fileName} onFileChange={onFileChange}/>
       </div>
         <Button name="Delete" onClick={onSubmit}/>
     </form>
