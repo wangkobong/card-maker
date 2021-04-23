a61a824de636ee33261dc3e7034bc5b0b429ca9d
wangkobong <61865579+wangkobong@users.noreply.github.com>
2021-04-23 23:29:16 +0900

    remove warnings and clean up

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -24,18 +24,17 @@ const Maker = ({ FileInput, authService, cardRepository }) => {
       setCards(cards);
     });
     return () => stopSync();
-  }, [userId]);
+  }, [userId, cardRepository]);
 
   useEffect(() => {
     authService.onAuthChange(user => {
       if (user) {
         setUserId(user.uid);
-        console.log(userId);
       } else {
         history.push('/');
       }
     });
-  });
+  }, [userId, history, authService]);
 
   const createOrUpdateCard = card => {
     setCards(cards => {
